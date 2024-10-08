03481e049400abbe9f38520ae68de51ad1b8ace4
Otcheskaya <tatotch27@gmail.com>
2024-10-08 22:36:47 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+import "../scss/style.scss"
+
+document.addEventListener("DOMContentLoaded", function()  {
+    const showAllButton = document.querySelector('.list__button_show-all');
+    const hideAllButton = document.querySelector('.list__button_hide-all');
+    const hiddenItems = document.querySelectorAll('.hidden')
+  
+    showAllButton.addEventListener('click', function() {
+      hiddenItems.forEach(item => {
+        item.classList.remove('hidden');
+      });
+      showAllButton.style.display = 'none';
+      hideAllButton.style.display = 'block';
+    });
+  
+    hideAllButton.addEventListener('click', function() {
+      hiddenItems.forEach(item => {
+        item.classList.add('hidden');
+      });
+      hideAllButton.style.display = 'none';
+      showAllButton.style.display = 'block';
+    });
+  });
+  
+
+let swiperInstance;
+function initSwiper() {
+  if (window.innerWidth <= 768 && !swiperInstance) {
+    swiperInstance = new Swiper('.swiper', {
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      slidesPerView: 2,
+      spaceBetween: 150,
+    });
+  } else if (window.innerWidth > 768 && swiperInstance) {
+    swiperInstance.destroy(true, true);
+    swiperInstance = null;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', initSwiper);
+
+window.addEventListener('resize', function () {
+  initSwiper();
+});
