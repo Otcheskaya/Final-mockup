8039176012ee7c4c271e78610d94b699f5ff4777
Otcheskaya <tatotch27@gmail.com>
2024-11-11 14:30:27 +0300

    Adding/deliting comments

--- src/js/main.js
+++ src/js/main.js
@@ -51,6 +51,8 @@ window.addEventListener("resize", function () {
   initSwiper();
 });
 
+// Hidden elements
+
 document.addEventListener("DOMContentLoaded", function () {
   const showAllButton = document.querySelector(".list__button_show");
   const hideAllButton = document.querySelector(".list__button_hide");
@@ -86,144 +88,124 @@ document.getElementById("toggleButton").addEventListener("click", function () {
 
 //Modal windows - navbar
 
-// Получаем элементы для первого модального окна
 const chatModal = document.getElementById("chatModal");
 const btnChat = document.getElementById("openChat");
 const btnCloseChat = document.getElementsByClassName("modal__close")[0];
 const overlayChat = document.querySelector(".blur_modal");
 
-// Получаем элементы для второго модального окна
 const modal = document.getElementById("myModal");
 const btnOpenModal = document.getElementById("openModal");
 const btnCloseModal = document.getElementsByClassName("modal__closed")[0];
 const blurModal = document.querySelector(".blur_modal");
 
-// Функция для открытия первого модального окна
 btnChat.onclick = function () {
   chatModal.style.display = "block";
   overlayChat.style.display = "block";
 };
 
-// Функция для закрытия первого модального окна
 btnCloseChat.onclick = function () {
   chatModal.style.display = "none";
   overlayChat.style.display = "none";
 };
 
-// Функция для открытия второго модального окна
 btnOpenModal.onclick = function () {
   modal.style.display = "block";
   blurModal.style.display = "block";
 };
 
-// Функция для закрытия второго модального окна
 btnCloseModal.onclick = function () {
   modal.style.display = "none";
   blurModal.style.display = "none";
 };
 
-// Обработчик кликов по окну
 window.onclick = function (event) {
-  // Проверяем клик по области наложения первого модального окна
   if (event.target == overlayChat) {
     chatModal.style.display = "none";
     overlayChat.style.display = "none";
   }
 
-  // Проверяем клик по области наложения второго модального окна
   if (event.target == blurModal) {
     modal.style.display = "none";
     blurModal.style.display = "none";
   }
 };
 
-// Modal windows - menu
+//Modal windows - menu
+
 const chatWindow = document.getElementById("chatModal");
 const chat = document.getElementById("chatOpen");
 const btnCloseWindow = document.getElementsByClassName("modal__close")[0];
 const blurChat = document.querySelector(".blur_modal");
 
-// Получаем элементы для второго модального окна
 const modalWindow = document.getElementById("myModal");
 const btnOpenWindow = document.getElementById("modalOpen");
 const closeModal = document.getElementsByClassName("modal__closed")[0];
 const blurTel = document.querySelector(".blur_modal");
 
-// Функция для открытия первого модального окна
 chat.onclick = function () {
   chatWindow.style.display = "block";
   blurChat.style.display = "block";
 };
 
-// Функция для закрытия первого модального окна
 btnCloseWindow.onclick = function () {
   chatWindow.style.display = "none";
   blurChat.style.display = "none";
 };
 
-// Функция для открытия второго модального окна
 btnOpenWindow.onclick = function () {
   modalWindow.style.display = "block";
   blurTel.style.display = "block";
 };
 
-// Функция для закрытия второго модального окна
 closeModal.onclick = function () {
   modalWindow.style.display = "none";
   blurTel.style.display = "none";
 };
 
-// Обработчик кликов по окну
 window.onclick = function (event) {
-  // Проверяем клик по области наложения первого модального окна
   if (event.target == blurChat) {
     chatWindow.style.display = "none";
     blurChat.style.display = "none";
   }
 
-  // Проверяем клик по области наложения второго модального окна
   if (event.target == blurModal) {
     modalWindow.style.display = "none";
     blurTel.style.display = "none";
   }
 };
 
-//
+// Mobile menu
 
 document.getElementById("burger").addEventListener("click", function () {
   const navbar = document.getElementById("navbar");
   const blurNavbar = document.querySelector(".blur");
 
-  // Переключаем состояние навигации
   if (navbar.style.display === "block") {
-    navbar.style.display = "none"; // Скрываем навигацию
-    blurNavbar.style.display = "none"; // Скрываем эффект размытия
+    navbar.style.display = "none";
+    blurNavbar.style.display = "none";
   } else {
-    navbar.style.display = "block"; // Показываем навигацию
-    blurNavbar.style.display = "block"; // Показываем эффект размытия
+    navbar.style.display = "block";
+    blurNavbar.style.display = "block";
   }
 });
 
-// Добавляем обработчик события для закрытия при нажатии на заблюренную область
 document.querySelector(".blur").addEventListener("click", function () {
   const navbar = document.getElementById("navbar");
   const blurNavbar = document.querySelector(".blur");
 
-  // Переключение состояния вне зависимости от разрешения
   navbar.style.display = "none";
   blurNavbar.style.display = "none";
 });
 
-// Добавляем обработчик события для изменения размера окна
 window.addEventListener("resize", function () {
   const navbar = document.getElementById("navbar");
   const blurNavbar = document.querySelector(".blur");
 
   if (window.innerWidth === 1440) {
-    navbar.style.display = "block"; // Показываем навигацию
-    blurNavbar.style.display = "block"; // Показываем эффект размытия
+    navbar.style.display = "block";
+    blurNavbar.style.display = "block";
   } else {
-    navbar.style.display = "none"; // Скрываем навигацию
-    blurNavbar.style.display = "none"; // Скрываем эффект размытия
+    navbar.style.display = "none";
+    blurNavbar.style.display = "none";
   }
 });
